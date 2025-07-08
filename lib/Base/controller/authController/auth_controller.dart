af7bff86259781323e051ae52fa184f8b8b40fa7
naveed_kk <munirazhar21@gmail.com>
2025-07-08 07:00:34 +0500

    .

--- lib/Base/controller/authController/auth_controller.dart
+++ lib/Base/controller/authController/auth_controller.dart
@@ -91,13 +91,15 @@ class SignInController extends GetxController {
 
     if (isEmailValid && isPasswordValid)
     // if (true)
-     {
-      final request = LoginModel(
-        email: "shahbazvidicraze@gmail.com",
-        // email: "naveedkaimkhami@gmail.com",
-        // email: "admin@lineup.com",
-        password: "12345678",
-      );
+    {
+      final request = LoginModel(email: email, password: password);
+
+      // final request = LoginModel(
+      //   email: "shahbazvidicraze@gmail.com",
+      //   // email: "naveedkaimkhami@gmail.com",
+      //   // email: "admin@lineup.com",
+      //   password: "12345678",
+      // );
 
       final response;
       if (email.toString() == "admin@lineup.com")
